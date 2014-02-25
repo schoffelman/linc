e7092e74767f61033251d22d08d37826c77becea
TimCarissa <tim.schoffelman@gmail.com>
2014-02-24 23:22:06 -0600

    updated the privacy policy in the footer

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 </div><!-- #main .wrapper -->
 <footer id="colophon" role="contentinfo">
     <div class="content container">
-        <div class="row copyright col-md-9">&copy; 2001 - <?php echo date("Y"); ?> Ludens Inc. &mdash; All Rights Reserved</div>
+        <div class="row copyright col-md-9">&copy; 2001 - <?php echo date("Y"); ?> Ludens Inc. &mdash; All Rights Reserved - <a href="/about-us/privacy-policy" title="Privacy Policy">Privacy Policy</a></div>
         <div class="bbb col-md-3">
         	<!-- a target="_blank" href="https://www.bbb.org/online/consumer/cks.aspx?id=109061511729"><img title="Click to verify BBB accreditation and to see a BBB report." border=0 src="/design/plain/images/bbbsealh2.gif" alt="Click to verify BBB accreditation and to see a BBB report." /></a -->
         	<iframe src="http://seal-nebraska.bbb.org/logo/sehzbus/iframe/ludens-300047291.html" width="100" height="38" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
