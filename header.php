a9377c6b3f8ed1ea3d772e715b15a5c8e907b9c7
ludensinc.com <ludensinc.com@n10.c04.mtsvc.net>
2014-03-15 03:55:42 -0700

    uncommented the GA code

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 <link rel="Shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
 
 <script type="text/javascript">
-/*
+
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', 'UA-1575848-1']);
     _gaq.push(['_setDomainName', 'ludensinc.com']);
@@ -53,7 +53,7 @@
         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
     })();
-*/
+
 </script>
 
 
